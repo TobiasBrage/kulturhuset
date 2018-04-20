45fddcc1fe6864785adf6e9a658172b96b38b321
Tobias Brage <gamerfreaq@live.com>
2018-04-20 12:56:19 +0200

    rettelse

--- diverse/dateToUnix.js
+++
@@ -1,2 +0,0 @@
-var d = '2018-02-12T13:45:00Z';
-console.log(new Date(d).valueOf());
--- diverse/timeStringToVariables.js
+++
@@ -1,6 +0,0 @@
-let time = 1845;
-time = time.toString();
-let hour = time.substring(0,2);
-let minute = time.substring(2,4);
-
-console.log(hour+':'+minute);
